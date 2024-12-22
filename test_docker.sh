82f5da4e983037c63ba615476a8587eee32e5487
David Mašek <davidmasek42@gmail.com>
2024-12-22 21:42:45 +0100

    fix test_docker for CI

--- test_docker.sh
+++ test_docker.sh
@@ -8,18 +8,23 @@ set -o pipefail  # Fail if any part of a pipeline fails
 echo "Starting Beacon test."
 echo "---------------------"
 
-# DOCKER_BUILDKIT=1 docker compose build beacon
+DOCKER_BUILDKIT=1 docker compose build beacon
+echo "Build successful."
+echo "-----------------"
+
 docker compose run --rm \
  -T \
  --entrypoint bash \
- -v $(pwd)/config.yaml:/root/beacon.yaml:ro \
- beacon <<EOF
+ -v $(pwd)/config.yaml:/root/beacon-staging.yaml:ro \
+ -e BEACON_EMAIL_PREFIX='[staging]' \
+ beacon -c '
 set -e
 /app/beacon start &
 curl -sS -X POST http://localhost:8088/beat/sly-fox
 curl -sS -X GET http://localhost:8088/status/sly-fox
-/app/beacon report --send-mail
-EOF
+/app/beacon report --send-mail --config-file /root/beacon-staging.yaml
+'
+
 TEST_RESULT=$?
 if [ "$TEST_RESULT" -eq "0" ]; then
     echo "Testing: SUCCESS"
