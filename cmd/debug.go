3d351c982dca7776ec5c7f7766e1496b6ef84aab
David Mašek <davidmasek42@gmail.com>
2025-02-15 23:59:03 +0100

    add log rotation to docker compose

--- cmd/debug.go
+++ cmd/debug.go
@@ -14,15 +14,23 @@ var debugCmd = &cobra.Command{
 	Short: "Development tools, use with caution",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		logger := logging.Get()
-		logger.Debugw("Debug message", "foo", 42)
-		logger.Infow("Info message", "foo", 42)
-		logger.Warnw("Warn message", "foo", 42)
-		logger.Errorw("Error message", zap.Error(fmt.Errorf("big bad")))
+		repeat, err := cmd.Flags().GetInt("repeat")
+		if err != nil {
+			return err
+		}
+		for range repeat {
+			logger.Debugw("Debug message", "foo", 42)
+			logger.Infow("Info message", "foo", 42)
+			logger.Warnw("Warn message", "foo", 42)
+			logger.Errorw("Error message", zap.Error(fmt.Errorf("big bad")))
+		}
 		cmd.Println("Done")
 		return nil
 	},
 }
 
 func init() {
+	debugCmd.Flags().Int("repeat", 1, "")
+
 	rootCmd.AddCommand(debugCmd)
 }
