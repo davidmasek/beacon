d00c532b1423aee7c19f4eee1c773059a3dd1cd8
David Mašek <davidmasek42@gmail.com>
2025-07-09 20:57:01 +0200

    remove/simplify commands
    The current direction is to reduce "extra" functionality, to streghten
    the main flow and reduce complexity. I've kept the `report` command for now, as it is nice for testing,
    but at least I've simplified.

--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -33,23 +33,3 @@ func TestStartServer(t *testing.T) {
 	output := outputBuffer.String()
 	require.Contains(t, output, SERVER_SUCCESS_MESSAGE)
 }
-
-func TestListServices(t *testing.T) {
-	// be sure to setup DB path before interacting with Beacon
-	setupDbPathEnv(t)
-
-	var outputBuffer bytes.Buffer
-	rootCmd.SetOut(&outputBuffer)
-	rootCmd.SetErr(&outputBuffer)
-
-	// list services
-	rootCmd.SetArgs([]string{"list"})
-	err := rootCmd.Execute()
-	require.NoError(t, err)
-
-	// verify no services known
-	// we should be running with empty DB
-	// if there is something already - abort
-	require.Empty(t, outputBuffer)
-
-}
--- cmd/report.go
+++ cmd/report.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/davidmasek/beacon/handlers"
@@ -12,38 +11,12 @@ import (
 var reportCmd = &cobra.Command{
 	Use:   "report",
 	Args:  cobra.ExactArgs(0),
-	Short: "Generate and optionally send report about current service status",
+	Short: "Generate and send report about current service status",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		sendMail, err := cmd.Flags().GetBool("send-mail")
-		if err != nil {
-			return err
-		}
-		reportName, err := cmd.Flags().GetString("name")
-		if err != nil {
-			return err
-		}
-		noMail, err := cmd.Flags().GetBool("no-mail")
-		if err != nil {
-			return err
-		}
-
-		if sendMail && noMail {
-			return fmt.Errorf("cannot set both --send-mail and --no-mail")
-		}
-
 		config, err := loadConfig(cmd)
 		if err != nil {
 			return err
 		}
-
-		if sendMail {
-			// todo: figure out options for SendMail
-			config.EmailConf.Enabled = "yes"
-		}
-		if noMail {
-			config.EmailConf.Enabled = "no"
-		}
-		config.ReportName = reportName
 
 		db, err := storage.InitDB(config.DbPath)
 		if err != nil {
@@ -60,8 +33,4 @@ var reportCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(reportCmd)
-
-	reportCmd.Flags().Bool("send-mail", false, "Send email notifications")
-	reportCmd.Flags().Bool("no-mail", false, "Do not send email notifications")
-	reportCmd.Flags().String("name", "report", "Report name")
 }
